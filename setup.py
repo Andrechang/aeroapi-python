0193b7f78a8e83c98fdc93f715d8b9180237303e
Deren S <derens99@gmail.com>
2023-05-25 17:27:45 -0400

    fixing requests

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements_path = pathlib.Path("requirements.txt")
 
 setuptools.setup(
     name="aeroapi-python",
-    version="0.0.11111",
+    version="0.0.3",
     description='FlightAware AeroAPI Caller for Python',
     long_description=readme_path.read_text(),
     long_description_content_type="text/markdown",
