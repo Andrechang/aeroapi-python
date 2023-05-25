3169d649e64f6811420a9cec71debad0e274247c
Deren S <derens99@gmail.com>
2023-05-25 03:39:24 -0400

    Fixing the readme issue

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='aeroapi-python',
     version='0.0.11',
     author='Deren S',
     author_email='derens99@gmail.com',
     description='FlightAware AeroAPI Caller for Python',
-    long_description='FlightAware AeroAPI Caller for Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # This field indicates that your long_description is in Markdown.
     packages=find_packages(),
     install_requires=[
         'requests',
