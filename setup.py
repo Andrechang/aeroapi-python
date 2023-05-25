2fa08122f6461b41633fc6f3c7297ab890dccd90
Deren S <derens99@gmail.com>
2023-05-25 03:31:34 -0400

    Fixed long desc

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='aeroapi-python',
-    version='0.0.1',
+    version='0.0.11',
     author='Deren S',
     author_email='derens99@gmail.com',
     description='FlightAware AeroAPI Caller for Python',
+    long_description=open('README.md').read(),
     packages=find_packages(),
     install_requires=[
         'requests',
     ],
 )
-    
