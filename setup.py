1f32308934c2daa339528aa2b073d8c6f26ecfe9
Deren S <derens99@gmail.com>
2023-05-28 12:53:07 -0400

    Pushing new to pypi

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements_path = pathlib.Path("requirements.txt")
 
 setuptools.setup(
     name="aeroapi-python",
-    version="1.0.3",
+    version="1.1.0",
     description='FlightAware AeroAPI Caller for Python',
     long_description=readme_path.read_text(),
     long_description_content_type="text/markdown",
