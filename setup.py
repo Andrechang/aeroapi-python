3bab083fb2e0a2f8cfe48712ec0543ba9faad621
Deren S <derens99@gmail.com>
2023-05-28 13:13:05 -0400

    Fixing pypi workflow again

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements_path = pathlib.Path("requirements.txt")
 
 setuptools.setup(
     name="aeroapi-python",
-    version="1.1.0",
+    version="1.1.01",
     description='FlightAware AeroAPI Caller for Python',
     long_description=readme_path.read_text(),
     long_description_content_type="text/markdown",
